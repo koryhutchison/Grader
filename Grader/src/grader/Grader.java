88df4e97007e61cc68840f21c115b236b505b068
koryhutchison <kory.hutchison@icloud.com>
2017-01-03 18:44:08 -0700

    Final Upload

---
+++ Grader/src/grader/Grader.java
@@ -0,0 +1,26 @@
+
+package grader;
+
+
+
+public class Grader {
+
+    public static void main(String[] args) {
+      // Student oStud = new Student();
+       
+       //oStud.firstName = "Mickey";
+       
+       // System.out.println(oStud.firstName);
+       
+       
+       int iCount = 0;
+ 
+        while (iCount < 5)
+        {
+        System.out.println(" iCount ");
+        iCount ++;
+        }
+        
+    }
+    
+}
---
+++ Grader/src/grader/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grader;
+
+/**
+ *
+ * @author koryhutchison
+ */
+public class Student 
+{
+    String firstName;
+    String lastName;
+    int exam1;
+    int exam2;
+    int exam3;
+    
+}
